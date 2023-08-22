72b9a7dd60da26ca0cf6bc12411f233112c3c965
“Mirza” <mirza_azmathullah@cognerium.com>
2023-08-22 16:18:48 +0530

    Polygon

--- lib/main.dart
+++ lib/main.dart
@@ -42,15 +42,18 @@ class MyCustomPainter extends CustomPainter{
   @override
   void paint(Canvas canvas, Size size) {
     // CREATING THE BRUSH
-    Paint brush=Paint()..strokeWidth=5..color=Colors.blueAccent..style = PaintingStyle.stroke;
+    Paint brush=Paint()..strokeWidth=5..color=Colors.blueAccent..style = PaintingStyle.fill;
 
     // CREATING THE PATH
     final shape=Path();
 
-     shape.moveTo( size.width*0.2, size.height*0.2);
-     shape.relativeLineTo(size.width*0.6, 0);
-     shape.relativeLineTo(0, size.height*0.7);
-    shape.relativeLineTo(-size.width*0.6, 0);
+    shape.moveTo(size.width/2,size.height*0.1);
+    shape.relativeLineTo(size.width*0.3,size.height*0.2);
+    shape.relativeLineTo(0, size.height*0.3);
+    shape.relativeLineTo(-size.width*0.3,size.height*0.2);
+    shape.relativeLineTo(-size.width*0.3,-size.height*0.2);
+    shape.relativeLineTo(0,-size.height*0.3);
+
      shape.close();
 
 
